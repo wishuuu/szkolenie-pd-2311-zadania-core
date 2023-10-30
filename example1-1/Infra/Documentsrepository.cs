32900f1db9487d39e9b7316c1f66d5c2dd53e904
Oskar Wiszowaty <ppwiszu@gmail.com>
2023-10-30 10:52:53 +0100

    Task 1 fix

--- example1-1/Infra/Documentsrepository.cs
+++ example1-1/Infra/Documentsrepository.cs
@@ -7,7 +7,7 @@ public class DocumentsRepository
     private List<Document> _documents = new List<Document>();
     public void CreateDocument(Document document)
     {
-        document.Id = _documents.Max(x => x.Id);
+        document.Id = _documents.Count > 0 ?_documents.Max(x => x.Id) : 1;
         _documents.Add(document);
     }
         
